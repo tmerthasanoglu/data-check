c765d2cd5ff3e2ba0bfd75043f632516b3840d52
Mert <temelmert53@gmail.com>
2025-12-16 18:02:58 +0300

    Solved the first challenge of the workintech :)

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 
-
+import math
 def circle_area(radius):
-    """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+   return math.pi * radius ** 2
+
+print(circle_area(5))
